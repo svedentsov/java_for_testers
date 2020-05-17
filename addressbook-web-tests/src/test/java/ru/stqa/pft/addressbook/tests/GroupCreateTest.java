ad25ac97806f9b37a7f2cc56fa1ef892f8f2ca71
Игорь Сведенцов <svedentsov@gmail.com>
2020-05-17 09:56:19 +0500

    Реализовано чтение данных из файла xml.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreateTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreateTest.java
@@ -1,13 +1,20 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import javax.xml.crypto.Data;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,12 +22,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreateTest extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups() {
-        List<Object[]> list = new ArrayList<>();
-        list.add(new Object[]{new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
-        list.add(new Object[]{new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
-        list.add(new Object[]{new GroupData().withName("test3").withHeader("header 3").withFooter("footer 3")});
-        return list.iterator();
+    public Iterator<Object[]> validGroups() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+        String xml = "";
+        String line = reader.readLine();
+        while (line != null) {
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(GroupData.class);
+        List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+        return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
 
     @Test(dataProvider = "validGroups")
