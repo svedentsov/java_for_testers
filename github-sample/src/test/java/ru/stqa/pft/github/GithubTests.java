2383b3fa242b648a70f16e9fd05d6397d0c98b25
Игорь Сведенцов <svedentsov@gmail.com>
2020-05-27 22:22:58 +0500

    Создан тест взаимодействующий с GitHub через Remote API

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+    @Test
+    public void testCommets() throws IOException {
+        Github github = new RtGithub(".. your OAuth token ..");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("svedentsov", "java_pft")).commits();
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
+    }
+}
