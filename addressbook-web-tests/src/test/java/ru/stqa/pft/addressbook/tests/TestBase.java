28ac56c1e05415c30dcdc2125e4557d540f268b4
Игорь Сведенцов <svedentsov@gmail.com>
2020-05-29 22:35:12 +0500

    Добавлена генерация отчётов при помощи Allure

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -3,15 +3,12 @@ package ru.stqa.pft.addressbook.tests;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
-import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -19,6 +16,7 @@ import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+//@Listeners(MyTestListener.class)
 public class TestBase {
 
     Logger logger = LoggerFactory.getLogger(TestBase.class);
@@ -26,8 +24,9 @@ public class TestBase {
     protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
-    public void setUp() throws IOException {
+    public void setUp(ITestContext context) throws Exception {
         app.init();
+        context.setAttribute("app", app);
     }
 
     @AfterSuite(alwaysRun = true)
